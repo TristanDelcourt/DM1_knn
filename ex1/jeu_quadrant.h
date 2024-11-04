e5c6a3a9922bbce949ecfcf56698ea1bc3655287
Tristan Delcourt <tristandelcourt826@gmail.com>
2024-11-04 23:17:44 +0100

    comments

--- ex1/jeu_quadrant.h
+++ ex1/jeu_quadrant.h
@@ -2,5 +2,8 @@
 #include "vector.h"
 #include "database.h"
 
+// Donne la classe du vecteur v en fonction de ses coordonnées
 int quadrant(vector v);
+
+// Crée une base de données de taille db_size contenant des vecteurs de taille 2 entre (0, 0) et (255, 255) avec leur classe
 database fabrique_jeu_donnees(int db_size);
--- ex1/knn.h
+++ ex1/knn.h
@@ -6,6 +6,22 @@
 
 extern int nb_classe_max;
 
+/*
+prend en arguments un jeu de données db, un entier k tel que db->size > k et un vecteur input, et retournant la liste des candidats dans db->datas des k plus proches vecteurs de input (parmi
+ceux de db->datas)
+*/
 candidats pproche(database data, int k, vector input);
+
+/*
+prend en arguments une liste de candidats lc de taille r et retournant la classe la plus présente
+parmi les r données classifiées dans db repérées par leur indice dans lc.
+*/
 int classe_majoritaire(database db, candidats lc, int r);
+
+/*
+prenant
+en arguments un jeu de données data, la taille du jeu data_size, un entier k tel que db->size >
+k et un vecteur input, et retournant la classe la plus présente parmi les k plus proches voisins
+de input.
+*/
 int classify(database db, int k, vector input);
